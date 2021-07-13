5c4e4c714f5a4dfef751b335316a58de9126bd9a
LZHOU04 <86981747+LZHOU04@users.noreply.github.com>
2021-07-12 19:05:34 -0500

    Update app.py
    Added some widgets

--- app.py
+++ app.py
@@ -3,23 +3,37 @@ from toga.style import Pack
 from toga.style.pack import COLUMN, ROW #do not remove
 
 
-class CongressionalAppChallenge2021(toga.App):
+class TestPassword(toga.App):
 
-    def startup(self): #Code for App
-        """
-        Construct and show the Toga application.
+    def startup(self):
+        main_box = toga.Box(style=Pack(direction=COLUMN))#creates starting box
 
-        Usually, you would add your application to a main content box.
-        We then create a main window (with a name matching the app), and
-        show the main window.
-        """
-        main_box = toga.Box()
+        password_label = toga.Label(#creates text "Password:"
+            'Password: ',
+            style=Pack(padding=(200, 5))#set at pos 200px, 5ox
+        )
+        self.password_input = toga.TextInput(style=Pack(padding=(200, 5), flex=1))
+        #sets place to text input
+        name_password = toga.Box(style=Pack(direction=ROW, padding=5))#creates box which contains text and input
+        name_password.add(password_label)#adds text and input
+        name_password.add(self.password_input)
 
-        self.main_window = toga.MainWindow(title=self.formal_name)
+        button = toga.Button(#creation of button with text "Test Password"
+            'Test Password',
+            on_press=self.returnpassword,#on press returns text in input
+            style=Pack(padding=(20, 5))#location of button
+        )
+
+        main_box.add(name_password)#adds box containing input and text to starting box
+        main_box.add(button)#adds button to box
+
+        self.main_window = toga.MainWindow(title=self.formal_name)#creates window
         self.main_window.content = main_box
         self.main_window.show()
 
+    def returnpassword(self, widget):
+        print(self.password_input.value)#prints the password inputted into console -- subject to change
 
 def main(): #do not remove
-    return CongressionalAppChallenge2021()
+    return TestPassword()
 
